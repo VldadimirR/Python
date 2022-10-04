d301d639d5ebf57256d7c9cdea41daeb7a36ad7d
VladimirR <Vova.ru0007@yandex.ru>
2022-10-04 12:58:26 +0500

    Добавил варианты 2 решения 1,2,3,4 задачи

--- Homework_3/Task3_1.py
+++ Homework_3/Task3_1.py
@@ -22,3 +22,29 @@ def Sum_of_numbers_list_odd_positions(amount_of_numbers):
 
 
 print(Sum_of_numbers_list_odd_positions(amount_of_numbers))
+
+# Вариант 2
+
+# from random import sample
+
+
+# def list_rand_nums(count: int):
+#     if count < 0:
+#         print("Negative value of the number of numbers!")
+#         return []
+
+#     list_nums = sample(range(1, count * 2), count)
+#     return list_nums
+
+
+# def sum_odd_pos(list_nums: list):
+#     sum_nums = 0
+#     for k in range(0, len(list_nums), 2):
+#         sum_nums += list_nums[k]
+#     return sum_nums
+
+
+# all_list = list_rand_nums(int(input("Number of numbers: ")))
+# print(all_list)
+# print(sum_odd_pos(all_list))
+
--- Homework_3/Task3_3.py
+++ Homework_3/Task3_3.py
@@ -20,3 +20,16 @@ while number > 0:
 
 list.reverse()
 print(list)
+
+# Вариант 2
+# def conv_bin(num: int):
+#     list_nums = []
+
+#     while num > 0:
+#         list_nums.insert(0, num % 2)
+#         num //= 2
+
+#     print(*list_nums, sep="")
+
+
+# conv_bin(int(input()))
