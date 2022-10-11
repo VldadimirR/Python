da9facc91faf7fbfc78333f5ad70d5e90855f270
VladimirR07 <Vova.ru0007@yandex.ru>
2022-10-11 13:22:38 +0500

    Добавил домашние задачи 3 и 4

--- Homework_5/Task5_1.py
+++ Homework_5/Task5_1.py
@@ -36,3 +36,27 @@ def find(my_List):
 result = name(number)
 print(result)
 print(find(result))
+
+# Вариант 2
+# from random import sample
+
+
+# def list_rand_words(count: int, alp: str = 'абв'):
+#     words_list = []
+#     for i in range(count):
+#         letters = sample(alp, 3)
+#         words_list.append("".join(letters))
+#     return " ".join(words_list)
+
+
+# # def list_rand_words(count: int, alp: str = 'абв'):
+# #     return " ".join("".join(sample(alp, 3)) for _ in range(count))
+
+
+# def simple_sentence(words: str) -> str:
+#     return " ".join(words.replace("абв", "").split())
+
+
+# all_list = list_rand_words(int(input("Number of words: ")))
+# print(all_list)
+# print(simple_sentence(all_list))
--- Homework_5/Task5_2.py
+++ Homework_5/Task5_2.py
@@ -18,10 +18,9 @@
 # 1v2b2w2P3u2T1Y1y2W2Q
 
 
-
-def encode(my_text):  
+def encode(my_text):
     str_code = ''
-    count = 1       
+    count = 1
     for i in range(len(my_text)):
         if i < len(my_text)-1:
             if my_text[i] == my_text[i + 1]:
@@ -34,7 +33,6 @@ def encode(my_text):
     return str_code
 
 
-
 def decode(line):
     count = ''
     result = ''
@@ -45,6 +43,7 @@ def decode(line):
             result += i * int(count)
             count = ''
     return result
+
 
 text = 'aaaaavvvvvvvvvvvvvvvvvvvvvvvvvvvvvssssDDDdddFFggggOOiiiaa'
 
@@ -62,3 +61,47 @@ with open('Homework_5/text_code_words.txt', 'w') as my_f:
 
 with open('Homework_5/text_code_words.txt', 'r') as my_f:
     my_text = my_f.read()
+
+#Вариант 2
+# from itertools import groupby, starmap
+# from os import path
+
+
+# def rle_encode(text="text_words.txt", code_text="text_code_words.txt"):
+#     if path.exists(text) and path.exists(code_text):
+#         with open(text) as my_f_1, \
+#                 open(code_text, "a") as my_f_2:
+#             for i in my_f_1:
+#                 my_f_2.write("".join([f"{len(list(v))}{ch}" for ch, v in groupby(i)]))
+#     else:
+#         print("The files do not exist in the system!")
+
+
+# def rle_decode(name):
+#     if path.exists(name):
+#         with open(name) as my_f:
+#             n = ""
+#             for k in my_f:
+#                 word_nums = []
+#                 for i in k.strip():
+#                     if i.isdigit():
+#                         n += i
+#                     else:
+#                         word_nums.append([int(n), i])
+#                         n = ""
+#                 print("".join(starmap(lambda x, y: x * y, word_nums)))
+#     else:
+#         print("The files do not exist in the system!")
+
+# # def rle_decode(name):
+# #     if path.exists(name):
+# #         with open(name) as my_f:
+# #             for i in my_f:
+# #                 word_nums = ["".join(g) for k, g in groupby(i.strip(), key=str.isdigit)]
+# #                 print("".join([f"{int(word_nums[i]) * word_nums[i + 1]}" for i in range(0, len(word_nums), 2)]))
+# #     else:
+# #         print("The files do not exist in the system!")
+
+# # aaaaavvvvvvvvvvvvvvvvvvvvvvvvvvvvvssssDDDdddFFggggOOiiiaa
+# rle_encode(input("Enter the name of the file with the text: "), input("Enter the file name to record: "))
+# rle_decode(input("Enter the name of the file to decode: "))
